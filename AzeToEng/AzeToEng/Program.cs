38747da1dc8bfd2ea5e2f7ef14bcfaef6b540ac5
Hatamli <kamilahat@code.edu.az>
2022-04-04 12:04:43 +0400

    first commit

---
+++ AzeToEng/AzeToEng/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AzeToEng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ölü, Əli, Şah, Çağır";
+            text = text.ToLower();
+            Console.WriteLine(AzeToEngConverter(text));
+        }
+
+        public static string AzeToEngConverter(string text)
+        {
+            text = text.Replace('ö', 'o');
+            text = text.Replace('ü', 'u');
+            text = text.Replace('ğ', 'g');
+            text = text.Replace('ş', 's');
+            text = text.Replace('ı', 'i');
+            text = text.Replace('ç', 'c');
+            text = text.Replace('ə', 'e');
+
+            return text;
+        }
+    }
+}
